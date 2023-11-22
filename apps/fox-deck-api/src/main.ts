108ab156f23c56afdc0bfe6035b532279ada5dff
Dominique Börner <dominique@mukosoft.de>
2023-11-22 16:56:23 +0100

    chore: generate swagger file

--- apps/fox-deck-api/src/main.ts
+++ apps/fox-deck-api/src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from "@nestjs/core";
 import { SwaggerModule, DocumentBuilder } from "@nestjs/swagger";
 import { AppModule } from "./app.module";
 import { ValidationPipe } from "@nestjs/common";
+import * as fs from "fs";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -15,6 +16,7 @@ async function bootstrap() {
     .setVersion("1.0")
     .build();
   const document = SwaggerModule.createDocument(app, config);
+  fs.writeFileSync("./swagger-spec.json", JSON.stringify(document));
   SwaggerModule.setup("api", app, document);
   await app.listen(3000);
 }
