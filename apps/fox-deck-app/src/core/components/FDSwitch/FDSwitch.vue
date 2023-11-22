cdb388fcd779a954b9b9047d02f3844d0a38fdf2
Dominique Börner <dominique@mukosoft.de>
2023-11-22 15:42:58 +0100

    chore: implement different FDSwitch sizes

--- apps/fox-deck-app/src/core/components/FDSwitch/FDSwitch.vue
+++ apps/fox-deck-app/src/core/components/FDSwitch/FDSwitch.vue
@@ -1,11 +1,22 @@
 <script setup lang="ts">
+import type { PropType } from "vue";
+import type { FDComponentSize } from "@/core/types/component.types";
+
 defineProps({
   checked: { type: Boolean, default: false },
+  size: {
+    type: Object as PropType<FDComponentSize>,
+    default: "medium" satisfies FDComponentSize,
+  },
 });
 </script>
 <template>
   <label
     class="relative h-8 w-14 cursor-pointer [-webkit-tap-highlight-color:_transparent]"
+    :class="{
+      'h-[22px] w-10': size === 'small',
+      'h-10 w-16': size === 'large',
+    }"
   >
     <input
       type="checkbox"
@@ -14,7 +25,11 @@ defineProps({
     />
 
     <span
-      class="absolute inset-y-0 start-0 z-10 m-1 inline-flex h-6 w-6 items-center justify-center rounded-full bg-white text-gray-400 transition-all peer-checked:start-6 peer-checked:text-success-darker"
+      class="absolute inset-y-0 start-0 z-10 m-1 inline-flex h-6 aspect-square items-center justify-center rounded-full bg-white text-gray-400 transition-all peer-checked:start-6 peer-checked:text-success-darker"
+      :class="{
+        'h-[18px] m-[2px] peer-checked:start-[18px]': size === 'small',
+        'h-8 m-1 peer-checked:start-6': size === 'large',
+      }"
     >
       <vue-feather v-if="checked" type="check" size="14" />
       <vue-feather v-else type="x" size="14" />
---
+++ apps/fox-deck-app/src/core/types/component.types.ts
@@ -0,0 +1,9 @@
+/**
+ * This file contains TypeScript types which every component can use and implement,
+ * to contain a stable API trough every component.
+ */
+
+/**
+ * Type which indicates, which type of sizes a component can use.
+ */
+export type FDComponentSize = "small" | "medium" | "large";
