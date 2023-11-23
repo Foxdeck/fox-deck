04cdd0e72ec19c169817b95ebcb83b81c74fe29b
Dominique Börner <dominique@mukosoft.de>
2023-11-23 11:06:38 +0100

    chore: update JSDoc

--- apps/fox-deck-app/src/core/composables/useOpenAI.ts
+++ apps/fox-deck-app/src/core/composables/useOpenAI.ts
@@ -1,15 +1,20 @@
 import OpenAI from "openai";
-import { useQuestionnaireStore } from "@/core/stores/questionnaire.store";
 
+/**
+ * Composable for interacting with openAIs API for ChatGPT.
+ */
 export function useOpenAI() {
-  const questionnaireStore = useQuestionnaireStore();
-
   const openAI: OpenAI = new OpenAI({
     apiKey: import.meta.env.VITE_OPENAI_KEY,
     dangerouslyAllowBrowser: true,
   });
 
-  const generateQuestions = async (topic: string) => {
+  /**
+   * // TODO: this should be refactored to be in the backend.
+   * Generate questions from ChatGPT.
+   * @param topic {string} the topic which the user wants questions from.
+   */
+  const generateQuestions = async (topic: string): Promise<any> => {
     const prompt = `
             Generiere 10 Fragen zu folgendem Thema: ${topic}
     
@@ -27,8 +32,7 @@ export function useOpenAI() {
     });
 
     const lastIndex = response.choices.length - 1;
-    questionnaireStore.questions = response.choices[lastIndex].message
-      .content as any;
+    return response.choices[lastIndex].message.content as any;
   };
 
   return {
