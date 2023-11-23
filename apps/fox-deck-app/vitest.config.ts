9de92fec965d87baa94f6464d0114d11b8951bbf
Dominique Börner <dominique@mukosoft.de>
2023-11-23 10:32:58 +0100

    test: add test runner and coverage report for vitest

--- apps/fox-deck-app/vitest.config.ts
+++ apps/fox-deck-app/vitest.config.ts
@@ -2,13 +2,24 @@ import { fileURLToPath } from "node:url";
 import { mergeConfig, defineConfig, configDefaults } from "vitest/config";
 import viteConfig from "./vite.config";
 
+const customExcludes = ["**/api.ts"];
+
 export default mergeConfig(
   viteConfig,
   defineConfig({
     test: {
+      watch: false,
       environment: "jsdom",
-      exclude: [...configDefaults.exclude, "e2e/*"],
+      exclude: [...configDefaults.exclude, "e2e/*", ...customExcludes],
       root: fileURLToPath(new URL("./", import.meta.url)),
+      coverage: {
+        enabled: true,
+        exclude: [...customExcludes],
+        provider: "istanbul",
+        reporter: ["html"],
+      },
+      reporters: ["html"],
+      outputFile: "./test-report/test-output.html",
     },
   }),
 );
