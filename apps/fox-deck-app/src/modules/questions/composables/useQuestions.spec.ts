584a890f523de2f2b52d9cdfbd41a3b3e979ceed
Dominique Börner <dominique@mukosoft.de>
2023-11-23 13:14:59 +0100

    chore: add create and update methods for questions

--- apps/fox-deck-app/src/modules/questions/composables/useQuestions.spec.ts
+++ apps/fox-deck-app/src/modules/questions/composables/useQuestions.spec.ts
@@ -19,6 +19,76 @@ describe("useQuestions", () => {
     fetchMocker.resetMocks();
   });
 
+  describe("createQuestion", () => {
+    it("should create a new question", async () => {
+      const spy = vi.spyOn(api.question, "questionControllerCreateQuestion");
+      const { createQuestion } = useQuestions();
+      const question = {
+        question: "What is 3+3?",
+        solution: "6",
+        isPublic: false,
+      };
+
+      await createQuestion(question);
+
+      expect(spy).toHaveBeenCalledWith(question);
+    });
+
+    it("should show a notification, if response has an error", async () => {
+      fetchMocker.mockReject({
+        name: "FetchError",
+        message: "Error while creating question.",
+      });
+      const notificationStore = useNotificationStore();
+      const { createQuestion } = useQuestions();
+      const question = {
+        question: "What is 3+3?",
+        solution: "6",
+        isPublic: false,
+      };
+
+      await createQuestion(question);
+
+      expect(notificationStore.notifications.length).toEqual(1);
+    });
+  });
+
+  describe("updateQuestion", () => {
+    it("should update a question", async () => {
+      const spy = vi.spyOn(api.question, "questionControllerUpdateQuestion");
+      const { updateQuestion } = useQuestions();
+      const questionId = "c674f44d-959f-4d34-9494-f917d2caea2d";
+      const question = {
+        question: "What is 3+3?",
+        solution: "6",
+        isPublic: false,
+      };
+
+      await updateQuestion(questionId, question);
+
+      expect(spy).toHaveBeenCalledWith(questionId, question);
+    });
+
+    it("should show a notification, if response has an error", async () => {
+      fetchMocker.mockReject({
+        name: "FetchError",
+        message: "Error while updating question.",
+      });
+      const notificationStore = useNotificationStore();
+      const { updateQuestion } = useQuestions();
+      const questionId = "c674f44d-959f-4d34-9494-f917d2caea2d";
+      const question = {
+        question: "What is 3+3?",
+        solution: "6",
+        isPublic: false,
+      };
+
+      await updateQuestion(questionId, question);
+
+      expect(notificationStore.notifications.length).toEqual(1);
+    });
+  });
+
   describe("fetchQuestions", () => {
     it("should update the question state", async () => {
       fetchMocker.mockResponseOnce(JSON.stringify(questionsMock));
--- apps/fox-deck-app/src/modules/questions/composables/useQuestions.ts
+++ apps/fox-deck-app/src/modules/questions/composables/useQuestions.ts
@@ -1,15 +1,55 @@
 import { api } from "@/core/services";
 import { useQuestionsStore } from "@/modules/questions/stores/questions.store";
 import { useNotificationStore } from "@/core/stores/notification.store";
+import type { QuestionDto } from "@/core/services/api";
 
+/**
+ * Composable which abstracts the CRUD operations for questions to the backend.
+ */
 export function useQuestions() {
   const { addNotification } = useNotificationStore();
   const { updateQuestions } = useQuestionsStore();
 
   /**
+   * Create a new question.
+   * @param question {QuestionDto} the question to create.
+   */
+  async function createQuestion(question: QuestionDto): Promise<void> {
+    try {
+      await api.question.questionControllerCreateQuestion(question);
+    } catch (e) {
+      addNotification({
+        title: "Fehler beim Erstellen der Frage",
+        text: "Bitte aktualisieren Sie die Seite oder versuchen Sie es später noch einmal.",
+        severity: "danger",
+      });
+    }
+  }
+
+  /**
+   * Update an already existing question by its id.
+   * @param questionId {string} the question to update.
+   * @param question {QuestionDto} the updated question.
+   */
+  async function updateQuestion(
+    questionId: string,
+    question: QuestionDto,
+  ): Promise<void> {
+    try {
+      await api.question.questionControllerUpdateQuestion(questionId, question);
+    } catch (e) {
+      addNotification({
+        title: "Fehler beim Aktualisieren der Frage",
+        text: "Bitte aktualisieren Sie die Seite oder versuchen Sie es später noch einmal.",
+        severity: "danger",
+      });
+    }
+  }
+
+  /**
    * Fetch questions and update the state.
    */
-  async function fetchQuestions() {
+  async function fetchQuestions(): Promise<void> {
     try {
       const response = await api.question.questionControllerGetQuestions();
       updateQuestions(response.data);
@@ -46,6 +86,8 @@ export function useQuestions() {
   }
 
   return {
+    createQuestion: createQuestion,
+    updateQuestion: updateQuestion,
     fetchQuestions: fetchQuestions,
     searchQuestions: searchQuestions,
   };
