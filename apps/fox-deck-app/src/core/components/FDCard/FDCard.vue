e62d30ae95a22fbee4c4c6f6f428274cfcc3f5fc
Dominique Börner <dominique@mukosoft.de>
2023-12-02 12:43:53 +0100

    feat: implement basic card component

---
+++ apps/fox-deck-app/src/core/components/FDCard/FDCard.vue
@@ -0,0 +1,39 @@
+<script setup lang="ts">
+import FDCardHeader from "@/core/components/FDCard/FDCardHeader.vue";
+import FDCardBody from "@/core/components/FDCard/FDCardBody.vue";
+
+defineProps({
+  title: {
+    type: String,
+    required: false,
+    default: ""
+  },
+  subtitle: {
+    type: String,
+    required: false,
+    default: ""
+  },
+  headerIcon: {
+    type: String,
+    required: false,
+    default: undefined
+  },
+  text: {
+    type: String,
+    required: false,
+    default: ""
+  }
+});
+</script>
+<template>
+  <div class="bg-white shadow-sm rounded-md">
+    <slot name="header" />
+    <FDCardHeader
+      :title="title"
+      :subtitle="subtitle"
+      :header-icon="headerIcon"
+    />
+    <slot name="body" />
+    <FDCardBody :text="text" />
+  </div>
+</template>
---
+++ apps/fox-deck-app/src/core/components/FDCard/FDCardBody.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import FDTypography from "@/core/components/FDTypography/FDTypography.vue";
+
+defineProps({
+  text: {
+    type: String,
+    required: false,
+    default: ""
+  }
+});
+</script>
+<template>
+  <div
+    v-if="text"
+    class="p-4"
+  >
+    <FDTypography>
+      {{ text }}
+    </FDTypography>
+  </div>
+</template>
