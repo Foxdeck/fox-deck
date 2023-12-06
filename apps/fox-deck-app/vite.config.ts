6932c4ada7ba43af68d3da9511896b6c4c598d40
Dominique Börner <dominiquebr46@gmail.com>
2023-12-06 10:09:01 +0100

    fix: make icons visible again

--- apps/fox-deck-app/vite.config.ts
+++ apps/fox-deck-app/vite.config.ts
@@ -6,13 +6,7 @@ import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue({
-    template: {
-      compilerOptions: {
-        isCustomElement: (tag) => ['vue-feather'].includes(tag),
-      }
-    }
-  }), svgLoader()],
+  plugins: [vue(), svgLoader()],
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
