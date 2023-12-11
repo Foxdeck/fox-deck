114913a176cf6d4ba0011017df2d898ee2978b61
Dominique Börner <dominique@mukosoft.de>
2023-12-11 16:44:53 +0100

    feat: add NotesList component and enhance notes functionality
    This commit adds a new NotesList component for displaying notes and expands existing notes functionality. It enhances note management by implementing CRUD operations and integrating with the backend API. An Editor now supports selecting a note & rendering its contents.

---
+++ apps/fox-deck-app/src/modules/notes/components/NotesList.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import {useNotesStore} from "@/modules/notes/stores/notes.store";
+
+const notesStore = useNotesStore();
+</script>
+
+<template>
+  <div class="flex flex-col gap-2 w-full max-w-[200px]">
+    <div
+      v-for="note in notesStore.notes"
+      :key="note.id"
+      class="cursor-pointer underline"
+      @click="notesStore.selectedNote = note"
+    >
+      {{ note.title }}
+    </div>
+  </div>
+</template>
