4f997fd205363fe408e5b1350f4e71386938631a
Dominique Börner <dominiquebr46@gmail.com>
2024-02-24 10:36:55 +0100

    refactor: Streamline folder creation in ResourceService
    The folder creation process in ResourceService has been refactored to make the code more readable and maintainable. Creation, user association, and logging details have been separated into individual functions. A new helper function has been introduced to generate input for folder creation.

--- apps/fox-deck-api/src/modules/resource/resource.service.ts
+++ apps/fox-deck-api/src/modules/resource/resource.service.ts
@@ -1,42 +1,83 @@
-import {Injectable, Logger} from "@nestjs/common";
-import {CreateFolderRequestDto} from "./dto/create-folder.dto";
-import {PrismaService} from "../../shared/services/prisma.service";
-import {Prisma, Resource} from "@prisma/client";
-import {ResourceTypes} from "./resource-types.enum";
+import { Injectable, Logger } from "@nestjs/common";
+import { CreateFolderRequestDto } from "./dto/create-folder.dto";
+import { PrismaService } from "../../shared/services/prisma.service";
+import { Prisma, Resource } from "@prisma/client";
+import { ResourceTypes } from "./resource-types.enum";
 
+/**
+ * Service class for managing resources.
+ */
 @Injectable()
 export class ResourceService {
     private readonly logger = new Logger(ResourceService.name);
-    private readonly prisma: PrismaService;
 
-    constructor(prisma: PrismaService) {
-        this.prisma = prisma;
-    }
+    constructor(private readonly prisma: PrismaService) { }
 
-    public async createFolder(folder : CreateFolderRequestDto, userId: string): Promise<Resource> {
+    /**
+     * Creates a folder with the given information.
+     *
+     * @param folder - The folder information to create.
+     * @param userId - The ID of the user creating the folder.
+     * @returns A Promise that resolves to the created folder.
+     */
+    public async createFolder(folder: CreateFolderRequestDto, userId: string): Promise<Resource> {
+        const createdAt = new Date();
+        const folderCreateInput: Prisma.ResourceCreateInput = this.generateFolderCreationInput(folder, createdAt);
+
         try {
-            const createdAt = new Date();
-            const folderCreateInput: Prisma.ResourceCreateInput = {
-                name: folder.name,
-                type: ResourceTypes.FOLDER,
-                createdAt
-            };
-            const createdFolder = await this.prisma.resource.create({
-                data: folderCreateInput
-            });
+            const createdFolder = await this.createResource(folderCreateInput);
+            await this.associateUserToResource(userId, createdFolder.resourceId);
 
-            // insert into UserResourceAssociation
-            await this.prisma.userResourceAssociation.create({
-                data: {
-                    userId,
-                    resourceId: createdFolder.resourceId
-                }
-            });
-
-            this.logger.debug("Folder was created successfully");
+            this.logger.debug("Folder created successfully");
             return createdFolder;
         } catch (error) {
             this.logger.error("Error while creating folder", error.stack);
         }
+    }
+
+    /**
+     * Generates the input for creating a folder resource.
+     *
+     * @param {CreateFolderRequestDto} folder - The folder object containing the name of the folder.
+     * @param {Date} createdAt - The creation date of the folder.
+     * @returns {Prisma.ResourceCreateInput} - The input object for creating a folder resource.
+     * @private
+     */
+    private generateFolderCreationInput(folder: CreateFolderRequestDto, createdAt: Date): Prisma.ResourceCreateInput {
+        return {
+            name: folder.name,
+            type: ResourceTypes.FOLDER,
+            createdAt,
+        };
+    }
+
+    /**
+     * Creates a resource.
+     *
+     * @param {Prisma.ResourceCreateInput} resourceCreateInput - The input data to create the resource.
+     * @returns {Promise<Resource>} A promise that resolves with the created resource.
+     * @private
+     */
+    private async createResource(resourceCreateInput: Prisma.ResourceCreateInput): Promise<Resource> {
+        return this.prisma.resource.create({ data: resourceCreateInput });
+    }
+
+    /**
+     * Associates a user to a resource.
+     *
+     * @param {string} userId - The ID of the user.
+     * @param {string} resourceId - The ID of the resource.
+     *
+     * @return {Promise<any>} - A promise that resolves with the created user resource association.
+     *
+     * @private
+     */
+    private async associateUserToResource(userId: string, resourceId: string) {
+        return this.prisma.userResourceAssociation.create({
+            data: {
+                userId,
+                resourceId
+            },
+        });
     }
 }
