a6dabee754f72afce9ac3cf182fe855a516d42a3
Dominique Börner <dominiquebr46@gmail.com>
2024-05-17 23:03:12 +0200

    WIP: Integration Test

--- apps/fox-deck-api-go/database/connection.go
+++ apps/fox-deck-api-go/database/connection.go
@@ -2,6 +2,7 @@ package database
 
 import (
 	"database/sql"
+	"fmt"
 	"fox-deck-api/logging"
 	_ "github.com/go-sql-driver/mysql"
 	"sync"
@@ -9,8 +10,11 @@ import (
 )
 
 type Connection struct {
-	DatabasePath string
-	connection   *sql.DB
+	Ip         string
+	Port       string
+	Username   string
+	Password   string
+	connection *sql.DB
 }
 
 var instance *Connection
@@ -21,7 +25,10 @@ var once sync.Once
 func GetInstance() *Connection {
 	once.Do(func() {
 		instance = &Connection{
-			DatabasePath: GetDevDatabasePath(),
+			Ip:       "localhost",
+			Port:     "3306",
+			Username: "root",
+			Password: "fdadmin",
 		}
 	})
 
@@ -33,7 +40,7 @@ func GetInstance() *Connection {
 // If the connection already is established, use these instance.
 func (con *Connection) Connect() *sql.DB {
 	// todo: put login data into .env file!
-	dsn := "root:fdadmin@tcp(localhost:3306)/foxdeck?parseTime=true"
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/foxdeck?parseTime=true", con.Username, con.Password, con.Ip, con.Port)
 	db, err := sql.Open("mysql", dsn)
 	if err != nil {
 		panic("Error opening database connection: " + err.Error())
--- apps/fox-deck-api-go/repository/user_repository_test.go
+++ apps/fox-deck-api-go/repository/user_repository_test.go
@@ -1,13 +1,90 @@
 package repository
 
 import (
+	"context"
+	"fox-deck-api/database"
 	it "fox-deck-api/testing"
+	"log"
 	"testing"
 )
 
-func TestCreateUser(t *testing.T) {
-	setup := it.IntegrationTestSetup{
-		//AdditionalScripts: &[]string{it.GetMigrationFile("foo")},
+func TestUserRepository_CreateUser_Success(t *testing.T) {
+	setup := it.IntegrationTestSetup{}
+	container := it.IntegrationTestEnvironment.Startup(&setup)
+
+	host, _ := container.Container.Host(context.Background())
+	mappedPort, _ := container.Container.MappedPort(context.Background(), "3306")
+	t.Logf("Container started at: %s:%s", host, mappedPort.Port())
+
+	userId := "1d978281-5ffc-48e8-ae40-5c85ab553816"
+	username := "1d978281-5ffc-48e8-ae40-5c85ab553816"
+	email := "1d978281-5ffc-48e8-ae40-5c85ab553816"
+	password := "Secur3P@ssw0rd!"
+	user := database.User{
+		Id:       userId,
+		Username: username,
+		Email:    email,
+		Password: password,
 	}
-	_ = it.IntegrationTestEnvironment.Startup(&setup)
+
+	defer container.Terminate(context.Background())
+
+	userRepoConn := &UserRepositoryConnection{
+		DbConnection: &database.Connection{
+			Ip:       host,
+			Port:     mappedPort.Port(),
+			Username: "root",
+			Password: "fdadmin",
+		},
+	}
+
+	insertedUserId, err := userRepoConn.InsertUser(user)
+	if err != nil {
+		t.Errorf("Error inserting user: %v", err)
+	}
+
+	if *insertedUserId != userId {
+		t.Errorf("Expected user ID to be returned, got nil")
+	}
+
+	query := `SELECT * FROM User
+        	  WHERE id = ?;`
+
+	stmt, err := userRepoConn.DbConnection.Connect().Prepare(query)
+	if err != nil {
+		t.Fatalf("Error preparing query: %v", err)
+	}
+
+	result, err := stmt.Query(userId)
+	if result.Err() != nil || err != nil {
+		t.Fatalf("Error executing query: %v", result.Err())
+	}
+
+	defer result.Close()
+
+	retrievedUser := &database.User{}
+	for result.Next() {
+		user := &database.User{}
+		if err := result.Scan(&user.Id, &user.Email, &user.Username, &user.Password); err != nil {
+			log.Fatal(err)
+		}
+
+		retrievedUser = user
+	}
+
+	if retrievedUser.Id != userId {
+		t.Errorf("Expected user ID to be %s, got %s", userId, retrievedUser.Id)
+	}
+
+	if retrievedUser.Email != email {
+		t.Errorf("Expected email to be %s, got %s", email, retrievedUser.Email)
+	}
+
+	if retrievedUser.Username != username {
+		t.Errorf("Expected username to be %s, got %s", username, retrievedUser.Username)
+	}
+
+	if retrievedUser.Password == password {
+		t.Errorf("Expected password to be encrypted, got %s", retrievedUser.Password)
+	}
 }
